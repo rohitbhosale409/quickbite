06a58fb443b13174050b50fad7ee0df4537258d7
rohitbhosale409 <rohitbhosale409@gmail.com>
2025-12-29 17:24:35 +0530

    Fix header issue and update layout

--- src/components/Loyouts/Header.js
+++ src/components/Loyouts/Header.js
@@ -2,30 +2,32 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "../../styles/HeaderStyle.css";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo/logo.png"
+import logo from "../../assets/logo/logo.png";
 
 function Header() {
     return (
-        <Header>
-            <Navbar collapseOnSelect expand="lg">
+        <header>
+            <Navbar collapseOnSelect expand="lg" className="navbar">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand>
                         <Link to="/" className="logo">
                             <img src={logo} alt="logo" className="img-fluid" />
                         </Link>
                     </Navbar.Brand>
+
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/about">about</Nav.Link>
-                            <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
-                            <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
-                            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-                            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                            <Nav.Link as={Link} to="/" className="nav-Link">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/about" className="nav-Link">About</Nav.Link>
+                            <Nav.Link as={Link} to="/menu" className="nav-Link">Our Menu</Nav.Link>
+                            <Nav.Link as={Link} to="/shop" className="nav-Link">Shop</Nav.Link>
+                            <Nav.Link as={Link} to="/blog" className="nav-Link">Blog</Nav.Link>
+                            <Nav.Link as={Link} to="/contact" className="nav-Link">Contact</Nav.Link>
+
                             <Nav.Link as={Link} to="/">
                                 <div className="cart">
-                                    <i class="bi bi-bag"></i>
+                                    <i className="bi bi-bag"></i>
                                     <em className="roundpoint">2</em>
                                 </div>
                             </Nav.Link>
@@ -33,7 +35,7 @@ function Header() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </Header>
+        </header>
     );
 }
 
