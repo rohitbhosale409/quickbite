d8effaa167b46211dcd3d421a6c9f51f07f91ec5
rohitbhosale409 <rohitbhosale409@gmail.com>
2026-01-05 17:13:59 +0530

    Updates section4

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,12 @@ import React from "react";
 import Layout from "../../components/Loyouts/Loyout.js";
 import "../../styles/HomeStyle.css";
 import Section1 from "./Section1";
-// import Section2 from "./Section2";
-// import Section3 from "./Section3";
-// import Section4 from "./Section4";
-// import Section5 from "./Section5";
-// import Section6 from "./Section6";
-// import Section7 from "./Section7";
+import Section2 from "./Section2";
+import Section3 from "./Section3";
+import Section4 from "./Section4";
+import Section5 from "./Section5";
+import Section6 from "./Section6";
+import Section7 from "./Section7";
 
 const Home = () => {
     return (
@@ -17,22 +17,22 @@ const Home = () => {
                 <Section1 />
 
                 {/* Home Section About */}
-                {/* <Section2 /> */}
+                <Section2 />
 
                 {/* Home Section Menu */}
-                {/* <Section3 /> */}
+                <Section3 />
 
                 {/* Home Section Promotion */}
-                {/* <Section4 /> */}
+                <Section4 />
 
                 {/* Home Section Shop */}
-                {/* <Section5 /> */}
+                <Section5 />
 
                 {/* Home Section Blog */}
-                {/* <Section6 /> */}
+                <Section6 />
 
                 {/* Home Section Contact */}
-                {/* <Section7 /> */}
+                <Section7 />
             </Layout>
         </>
     );
